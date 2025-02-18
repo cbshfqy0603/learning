a404be8a72a367767a3f170b957e2ff020589619
花繁庆云 <350445844@qq.com>
2025-02-18 16:02:51 +0800

    Add files via upload

---
+++ Arithmeticoperator/ArithmeticoperatorDemo2.java
@@ -0,0 +1,17 @@
+package Arithmeticoperator;
+
+public class ArithmeticoperatorDemo2 {
+    public static void main(String[] args) {
+        // /
+        //整数参与运算，其结果也是整数
+        //小数参与运算，其结果可能是不精确的
+        System.out.println(10 / 5);
+        System.out.println(10 / 3);
+        System.out.println(10.0 / 3);
+
+        //取模，取余数，也是也是除法运算，只不过最终的结果是余数而已
+        System.out.println(10 % 2);
+        System.out.println(10 % 3);
+
+    }
+}
