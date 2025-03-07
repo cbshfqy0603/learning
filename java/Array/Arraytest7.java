2720d608c750da5db555631ee32a78bb1f7e7b6e
花繁庆云 <350445844@qq.com>
2025-03-07 19:41:23 +0800

    Add files via upload

--- java/Array/Arraytest7.java
+++ java/Array/Arraytest7.java
@@ -1,19 +1,22 @@
 package Array;
 
-public class Arraytest6 {
+import java.util.Random;
+
+public class Arraytest7 {
     public static void main(String[] args) {
-        //已知一个数组，其中的元素为{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-        //交换前:1 2 3 4 5 6 7 8 9 10
-        //交换后:10 9 8 7 6 5 4 3 2 1
+        //打乱数据
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Random r = new Random();
+        //未打乱前：
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
-        for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+        for (int i = 0; i < arr.length; i++) {
+            int randomindex = r.nextInt(5);
             int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+            arr[i] = arr[randomindex];
+            arr[randomindex] = temp;
         }
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
