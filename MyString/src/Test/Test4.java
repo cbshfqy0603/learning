527c78ab2ad2b38e19b5318c6237f1b673e8d8b0
花繁庆云 <350445844@qq.com>
2025-04-22 23:21:33 +0800

    Add files via upload

--- MyString/src/Test/Test4.java
+++ MyString/src/Test/Test4.java
@@ -1,5 +1,7 @@
 package Test;
 
+import java.util.Random;
+
 public class Test4 {
     public static void main(String[] args) {
        /*
@@ -8,15 +10,44 @@ public class Test4 {
        规则：
        长度为5 内容中是四位字母，一位数字
        其中数字只有一位，但是可以出现在任意位置 */
-        //利用数组存储52位字母（A - Z \ a - z）
+       //将a-z A-Z 一共52个大小写字母存入数组
         char[] arr = new char[52];
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < 52; i++) {
             if (i <= 25) {
+                //存储a-z
                 arr[i] = (char)(97 + i);
             } else {
+                //存储A-Z
                 arr[i] = (char)(65 + i - 26);
             }
         }
-        //随机生成4个字母（不区分大小写），并将其拼接在一起
+        //从数组中随机抽取4个字母，直接拼接即可
+        Random r = new Random();
+        String result = "";
+        for (int i = 0; i < 4; i++) {
+            //随机生成0-(arr.length - 1)索引
+            int index = r.nextInt(arr.length);
+            result = result + arr[index];
+        }
+        //从0-9中随便选一个数字拼接在末尾即可
+        int number = r.nextInt(10);
+        result = result + number;
+        //将生成的数字在5个位置随便选一个即可
+        result = disorganize(result);
+        //打印结果
+        System.out.println(result);
+    }
+    //我要干什么 随机选择数字的位置
+    //我需要什么 原字符串
+    //原方法调用处是否需要返回值 需要(String)
+    public static String disorganize(String str) {
+        char[] chs = str.toCharArray();
+        Random r = new Random();
+        int randomIndex = r.nextInt(str.length());
+        //数字元素在最后一位
+        char temp = chs[str.length() - 1];
+        chs[str.length() - 1] = chs[randomIndex];
+        chs[randomIndex] = temp;
+        return new String(chs);
     }
 }
