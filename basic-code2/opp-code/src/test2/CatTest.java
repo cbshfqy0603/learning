b7d5c02cfe68a70d432b1f799ea04913a2803004
花繁庆云 <350445844@qq.com>
2025-05-07 23:14:42 +0800

    Add files via upload

---
+++ basic-code2/opp-code/src/test2/CatTest.java
@@ -0,0 +1,12 @@
+package test2;
+
+public class CatTest {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        cat1.name = "Tom";
+        cat1.age = 1;
+        cat1.gender = "公";
+        cat1.sleep();
+        cat1.eat();
+    }
+}
