377b6d131bc3c3252b9436efe3882f23c914af42
花繁庆云 <350445844@qq.com>
2025-05-31 22:55:12 +0800

    Add files via upload

---
+++ basic-code2/opp-code/src/test2/Cat.java
@@ -0,0 +1,13 @@
+package test2;
+
+public class Cat {
+    String name;
+    int age;
+    String gender;
+    public void sleep() {
+        System.out.println("睡觉");
+    }
+    public void eat() {
+        System.out.println("吃东西");
+    }
+}
