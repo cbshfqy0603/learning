691966251e4f582c49f31f77a1ac59b1da95e53a
花繁庆云 <350445844@qq.com>
2025-08-29 22:03:36 +0800

    Add files via upload

---
+++ basic-code2/opp-code/src/test1/Phone.java
@@ -0,0 +1,15 @@
+package test1;
+
+public class Phone {
+    //属性(成员变量)
+    String brand;
+    double price;
+
+    //行为(就叫方法)(成员方法)
+    public void call() {
+        System.out.println("手机在打电话");
+    }
+    public void playGame() {
+        System.out.println("手机在玩游戏");
+    }
+}
