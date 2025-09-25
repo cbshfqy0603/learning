18fd79698b2e705b1605f26cd6099200aa0b607a
花繁庆云 <350445844@qq.com>
2025-09-25 21:53:29 +0800

    Delete opp-polymorphism directory

--- opp-polymorphism/src/a01polymorphism01/Administrator.java
+++
@@ -1,8 +0,0 @@
-package a01polymorphism01;
-
-public class Administrator extends Person {
-    @Override
-    public void show() {
-        System.out.println("管理员的姓名为" + getName() + ", " + "年龄为：" + getAge());
-    }
-}
--- opp-polymorphism/src/a01polymorphism01/Teacher.java
+++
@@ -1,8 +0,0 @@
-package a01polymorphism01;
-
-public class Teacher extends Person {
-    @Override
-    public void show() {
-        System.out.println("老师的姓名为" + getName() + ", " + "年龄为：" + getAge());
-    }
-}
