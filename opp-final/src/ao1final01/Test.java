1a64e71556931ae293c640c0b309a993f13a80de
花繁庆云 <350445844@qq.com>
2025-09-26 23:38:47 +0800

    Add files via upload

---
+++ opp-final/src/ao1final01/Test.java
@@ -0,0 +1,12 @@
+package ao1final01;
+
+public class Test {
+    /*
+    final修饰方法：
+        表明该方法是最终方法，不能被重写
+    final修饰类：
+        表明该类是最终类，不能被继承
+    final修饰变量：
+        叫做常量，只能被赋值一次
+     */
+}
